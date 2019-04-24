813a3a39caacf68856e1715f4cedaa80e423c0a7
Yeang Eason <crossgate_2000@hotmail.com>
2019-04-24 23:14:33 +0800

    pack axios

--- PandaPrime/PrimeJS/console.js
+++ PandaPrime/PrimeJS/console.js
@@ -2,20 +2,19 @@
     var app = new Vue({
         el: '#app',
         data: {
-            permissionList: [],
+            topPermissionList: [],
             activeIndex2: '1'
         },
         methods: {
             getTopPermissions: function () {
                 var $this = this;
-                axios({
-                    method: 'post',
-                    url: '/DefaultPage/GetTopPermissions'
+                $.axios({
+                    url: '/DefaultPage/GetTopPermissions',
                 }).then(function (response) {
-                    if (response != null) {
+                    if (response !== null) {
                         var data = response.data;
-                        if (data != null && data.length > 0) {
-                            $this.permissionList = data;
+                        if (data !== null && data.length > 0) {
+                            $this.topPermissionList = data;
                         }
                     }
                 }).catch(function (error) {
@@ -28,8 +27,21 @@
             handleClose: function (key, keyPath) {
                 console.log(key, keyPath);
             },
-            handleSelect: function (key, keyPath) {
-                console.log(key, keyPath);
+            onSelectTop: function (key, keyPath) {
+                var $this = this;
+                axios({
+                    method: 'post',
+                    url: '/DefaultPage/GetSidePermissions'
+                }).then(function (response) {
+                    if (response !== null) {
+                        var data = response.data;
+                        if (data !== null && data.length > 0) {
+                            $this.permissionList = data;
+                        }
+                    }
+                }).catch(function (error) {
+                    console.log(error);
+                });
             }
         },
         mounted: function () {
---
+++ PandaPrime/PrimeJS/extend.js
@@ -0,0 +1,71 @@
+﻿jQuery.extend({
+    $Post: function (options) {
+        var lists = {
+            url: options.url,
+            data: options.data || {},
+            type: options.type || "POST",
+            dataType: options.dataType || "JSON",
+            async: options.async === false ? false : true,
+            //contentType: options.contentType || "application/json",
+            cache: options.cache === false ? false : true
+        }
+        $.ajax({
+            type: lists.type,
+            dataType: lists.dataType,
+            url: lists.url,
+            beforeSend: function (XHR) {
+                XHR.setRequestHeader("Token", (localStorage.getItem("Token") || ""));
+            },
+            //contentType: lists.contentType,
+            data: lists.data,
+            async: lists.async,
+            cache: lists.cache,
+            success: function (data) {
+                if (lists.dataType.toUpperCase() == "XML") {
+                    data = $(data).text();
+                    data = JSON.parse(data);
+                    data = data.d == null ? data : data.d;
+                    if (typeof data != "object") {
+                        try {
+                            data = JSON.parse(data);
+                        } catch (e) {
+                            data = data;
+                        }
+                    }
+                } else if (lists.dataType.toUpperCase() == "JSON") {
+                    data = data.d == null ? data : data.d;
+                    if (data != "" && data != null) {
+                        if (typeof data != "object") {
+                            try {
+                                data = JSON.parse(data);
+                            } catch (e) {
+                                data = data;
+                            }
+                        }
+                    }
+                }
+                options.success && options.success(data);
+            },
+            complete: function (data) {
+                options.complete && options.complete(data);
+            }
+        }).always(function () {
+            options.always && options.always();
+        });
+
+    },
+    axios: function (options) {
+        var pms = {
+            method: options.method || "POST",
+            url: options.url,
+            data: options.data,
+            responseType: options.responseType || 'json'
+        }
+        return axios({
+            method: pms.method,
+            url: pms.url,
+            data: pms.data,
+            responseType: pms.responseType
+        });
+    }
+});
