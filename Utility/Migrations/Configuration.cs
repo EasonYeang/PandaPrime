f0d746dca4e92a9ef2ca8e0d48a4361f738cb378
EasonYeang <185241770@qq.com>
2019-04-24 13:26:49 +0800

    Enter key event.

--- Utility/Migrations/Configuration.cs
+++ Utility/Migrations/Configuration.cs
@@ -9,7 +9,8 @@ namespace Utility.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = false;
         }
 
         protected override void Seed(Utility.PrimeContext context)
