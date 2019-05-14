e868800aa8350e3fd381a411c22aeae666f540b7
EasonYeang <185241770@qq.com>
2019-05-14 17:31:38 +0800

    修改侧边栏

--- PandaPrime/PrimeJS/console.js
+++ PandaPrime/PrimeJS/console.js
@@ -2,45 +2,11 @@
     var app = new Vue({
         el: '#app',
         data: {
-            topPermissionList: [],
             sidePermissionList: [],
-            topActive: '',
             src: '../../BaseInfo/Universe/welcome.html'
         },
         methods: {
-            getTopPermissions: function () {
-                var $this = this;
-                $.axios({
-                    url: '/DefaultPage/GetTopPermissions',
-                }).then(function (response) {
-                    if (response !== null) {
-                        var data = response.data;
-                        if (data !== null && data.length > 0) {
-                            $this.topPermissionList = data;
-                            $this.topActive = data[0].SerialNumber.toString();
-                        }
-                    }
-                }).catch(function (error) {
-                    console.log(error);
-                });
-            },
-            onSelectSide: function (key) {
-                var $this = this;
-                $.axios({
-                    url: '/DefaultPage/GetSideLocation',
-                    data: { sn: key }
-                }).then(function (response) {
-                    if (response !== null) {
-                        var data = response.data;
-                        if (data !== null && data.length > 0) {
-                            $this.src = data + '?tS=' + getGuid();
-                        }
-                    }
-                }).catch(function (error) {
-                    console.log(error);
-                });
-            },
-            onSelectTop: function (key) {
+            getSidePermissionList: function (key) {
                 var $this = this;
                 $.axios({
                     url: '/DefaultPage/GetSidePermissions',
@@ -57,15 +23,39 @@
                 }).catch(function (error) {
                     console.log(error);
                 });
-            }
-        },
-        watch: {
-            topActive: function (val, oldVal) {
-                this.onSelectTop(val);
+            },
+            logOut: function () {
+                $.axios({
+                    url: '/DefaultPage/LogOut'
+                }).then((response) => {
+                    var data = response.data;
+                    if (data != null) {
+                        if (data.Flag === 1) {
+                            window.location = '/Login/Index';
+                        }
+                    }
+                }).catch((error) => { console.log(error) });
+            },
+            clickSide: function (filePath) {
+                console.log(filePath);
+                if (filePath != null) {
+                    if (filePath.indexOf('?') === -1) {
+                        filePath = `${filePath}?timeStamp=${timeStamp()}`;
+                    } else {
+                        filePath = `${filePath}&timeStamp=${timeStamp()}`;
+                    }
+                    this.src = filePath;
+                }
             }
         },
         mounted: function () {
-            this.getTopPermissions();
+            this.getSidePermissionList(1);
+            console.log(this);
+
+            window.layui.use('element', function () {
+                var element = layui.element;
+            });
         }
     });
+
 });
