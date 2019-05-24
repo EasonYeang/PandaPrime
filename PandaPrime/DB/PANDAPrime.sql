7cc8087edf845da1186dcdf577da3ac6f8405909
EasonYeang <185241770@qq.com>
2019-05-24 22:06:22 +0800

    524

---
+++ PandaPrime/DB/PANDAPrime.sql
@@ -0,0 +1,3 @@
+INSERT INTO PPermission(Name,SerialNumber,ParentSN,Level,FilePath,Icon,IsDelete) VALUES('权限管理',1,0,1,'','fa fa-link',0);
+INSERT INTO PPermission(Name,SerialNumber,ParentSN,Level,FilePath,Icon,IsDelete) VALUES('角色管理',2,1,2,'','fa fa-link',0);
+INSERT INTO PPermission(Name,SerialNumber,ParentSN,Level,FilePath,Icon,IsDelete) VALUES('目录管理',3,1,2,'','fa fa-link',0);
